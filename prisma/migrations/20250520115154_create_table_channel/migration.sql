2d586029a275e3dc9992d0a727b08e93394e3992
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-05-20 08:53:49 -0300

    feat: criação da tabela channel

--- prisma/migrations/20250520115154_create_table_channel/migration.sql
+++ prisma/migrations/20250520115154_create_table_channel/migration.sql
@@ -49,6 +49,33 @@ CREATE TABLE "cards" (
     CONSTRAINT "cards_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "collections" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "bio" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "channels" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "isLive" TEXT NOT NULL,
+    "bannerUrl" TEXT,
+    "logoUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "channels_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "collections_name_key" ON "collections"("name");
 
@@ -56,4 +83,7 @@ CREATE UNIQUE INDEX "collections_name_key" ON "collections"("name");
 CREATE UNIQUE INDEX "cards_name_key" ON "cards"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "cards_collectionId_key" ON "cards"("collectionId");
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
